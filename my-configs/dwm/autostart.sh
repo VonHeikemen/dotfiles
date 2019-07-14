86634c7dd377c0d7506bb1a243502c7ff7358ee5
Heiker Curiel <heikercuriel@gmail.com>
2019-07-14 14:30:11 -0400

    dwm - added dwm utils

---
+++ my-configs/dwm/autostart.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+dte(){
+  dte="$(date +"%A, %B %d | 🕒 %l:%M%p")"
+  echo -e "$dte"
+}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "🖪 $mem"
+}
+
+
+run xfce4-power-manager &
+run clipit &
+run volumeicon &
+run nm-applet &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+run nitrogen --restore &
+run xautolock -time 50 -locker blurlock &
+
+while true; do
+     xsetroot -name "$(mem) | $(dte)"
+     sleep 10s    # Update time every ten seconds
+done &
---
+++ my-configs/dwm/make.sh
@@ -0,0 +1,62 @@
+#! /usr/bin/env bash
+
+branches=(
+  "movestack"
+  "config"
+  "systray"
+  "autostart"
+  "pertag"
+  "attachbottom"
+  "bottomstack"
+  "noborder"
+  "singularborders"
+  "hide_vacant_tags"
+  "center"
+  "dullcenteredwindowname"
+)
+
+fullclean() {
+  make clean && \
+  rm -f config.h && \
+  git reset --hard origin/master
+}
+
+build() {
+  make && sudo make clean install
+}
+
+apply() {
+  for branch in "${branches[@]}"; do
+    echo "merging $branch"
+    git merge $branch -m $branch
+  done
+}
+
+push_patches() {
+  for branch in "${branches[@]}"; do
+    git push gh "$branch"
+  done
+}
+
+case "$@" in
+  fullclean) 
+    fullclean
+  ;;
+
+  build)
+    build
+  ;;
+
+  apply)
+    fullclean && apply
+  ;;
+
+  clean-build)
+    fullclean && apply && build  
+  ;;
+
+  push-patches|push_patches)
+    push_patches
+  ;;
+esac
+
