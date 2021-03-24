b538b67aaa6a894999093dbec5f69a40b802fe33
Heiker Curiel <heikercuriel@gmail.com>
2021-03-24 18:36:54 -0400

    shell - update env variables

--- my-configs/shell/aliases.sh
+++ my-configs/shell/aliases.sh
@@ -1,11 +1,13 @@
-export PATH=$PATH:$HOME/bin:$HOME/.config/npm/packages/bin
 export EDITOR='nvim'
 export PYTHONUSERBASE=~/.local
 export TERMINAL='kitty'
-export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!cache/*" --glob "!node_modules/*"'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!cache/*" --glob "!node_modules/*" --glob "!vendor/*"'
 export FZF_DEFAULT_OPTS='--layout=reverse --border' 
 export BAT_THEME='base16'
 export NNN_FCOLORS='c1e20402000506f7c6d6ab09'
+export DENO_INSTALL="$HOME/.deno"
+
+export PATH=$PATH:$HOME/bin:$HOME/.config/npm/packages/bin:$DENO_INSTALL/bin
 
 alias duh='du -d 1 -h'
 
