bca35e864a046bc28fa7af07bfe80ded6082a423
Heiker Curiel <heikercuriel@gmail.com>
2021-06-21 23:18:28 -0400

    subl - update keybindings

--- .config/sublime-text-3/Packages/User/NvMode/keybindings.py
+++ .config/sublime-text-3/Packages/User/NvMode/keybindings.py
@@ -2,34 +2,42 @@ leader = " "
 
 def keybinding(bind, **kwargs):
   command = kwargs.get('command')
-  command_mode = context['command_mode']
+
+  nv_mode_enabled = context['command_mode'][0]
+  normal_mode = context['command_mode']
   visual_mode = context['visual_mode']
   insert_mode = context['insert_mode']
 
   # Hacky stuff
   bind(["h"], "move_visual_mode_starting_point", visual_mode, context['selection_empty'], by="characters")
   bind(["k"], "move_visual_mode_starting_point", visual_mode, context['selection_empty'], by="lines")
-  bind(["a"], "better_a", command_mode, context['selection_empty'])
+  bind(["a"], "better_a", nv_mode_enabled, context['selection_empty'])
   bind(["ctrl+o"], "hacky_ctrl_o", insert_mode, context['selection_empty'])
 
   # App commands
-  bind([leader, ","], "edit_settings", command_mode[0], base_file="$packages/User/NvMode/keybindings.py")
-  bind([leader, "Q"], "nv_disable_and_exit", command_mode[0])
-  bind([leader, "q", "q"], "safer_quit", command_mode[0])
-  bind([leader, "w"], "save", command_mode[0], **{"async": True})
+  bind([leader, ","], "edit_settings", nv_mode_enabled, base_file="$packages/User/NvMode/keybindings.py")
+  bind([leader, "Q"], "nv_disable_and_exit", nv_mode_enabled)
+  bind([leader, "q", "q"], "safer_quit", nv_mode_enabled)
+  bind([leader, "w"], "save", nv_mode_enabled, **{"async": True})
 
-  bind(["[", "t"], "prev_view", command_mode[0])
-  bind(["]", "t"], "next_view", command_mode[0])
+  bind(["[", "t"], "prev_view", nv_mode_enabled)
+  bind(["]", "t"], "next_view", nv_mode_enabled)
 
-  bind(["[", "q"], "prev_result", command_mode[0])
-  bind(["]", "q"], "next_result", command_mode[0])
+  bind(["[", "q"], "prev_result", nv_mode_enabled)
+  bind(["]", "q"], "next_result", nv_mode_enabled)
 
-  bind([leader, "b", "b"], "otl_open_tab_list", command_mode[0])
-  bind([leader, "b", "l"], "next_view_in_stack", command_mode[0])
-  bind([leader, "b", "c"], "close", command_mode[0])
+  # Manage multi-select Tabs
+  bind(["[", "f"], "focus_to_left", nv_mode_enabled)
+  bind(["]", "f"], "focus_to_right", nv_mode_enabled)
+  bind(["[", "F"], "unselect_to_left", nv_mode_enabled)
+  bind(["]", "F"], "unselect_to_right", nv_mode_enabled)
+
+  bind([leader, "b", "b"], "otl_open_tab_list", nv_mode_enabled)
+  bind([leader, "b", "l"], "next_view_in_stack", nv_mode_enabled)
+  bind([leader, "b", "c"], "close", nv_mode_enabled)
 
   # Sidebar
-  bind([leader, "d", "d"], "use_sidebar", command_mode[0])
+  bind([leader, "d", "d"], "use_sidebar", nv_mode_enabled)
   bind([leader, "d", "d"], "use_sidebar", context['sidebar_focused'])
   bind(["h"], "move", context['sidebar_focused'], by="characters", forward=False)
   bind(["l"], "move", context['sidebar_focused'], by="characters", forward=True)
@@ -37,26 +45,28 @@ def keybinding(bind, **kwargs):
   bind(["j"], "move", context['sidebar_focused'], by="lines", forward=True)
 
   # Search
-  bind(["enter"], "show_overlay", command_mode[0], overlay="command_palette")
+  bind(["enter"], "show_overlay", nv_mode_enabled, overlay="command_palette")
 
-  bind([leader, "F"], "show_panel", command_mode[0], panel="find_in_files")
-  bind([leader, "f", "f"], "show_overlay", command_mode[0], overlay="goto", show_files=True)
-  bind([leader, "f", "s"], "show_overlay", command_mode[0], overlay="goto", text="@")
-  bind([leader, "f", "S"], "goto_symbol_in_project", command_mode[0])
+  bind([leader, "F"], "show_panel", nv_mode_enabled, panel="find_in_files")
+  bind([leader, "f", "f"], "show_overlay", nv_mode_enabled, overlay="goto", show_files=True)
+  bind([leader, "f", "s"], "show_overlay", nv_mode_enabled, overlay="goto", text="@")
+  bind([leader, "f", "S"], "goto_symbol_in_project", nv_mode_enabled)
 
-  bind([leader, "r"], "show_panel", command_mode[0], panel="replace", reverse=False)
+  bind([leader, "r"], "show_panel", nv_mode_enabled, panel="replace", reverse=False)
 
-  bind([leader, "y"], "slurp_find_string", command_mode[0])
+  bind([leader, "y"], "slurp_find_string", nv_mode_enabled)
 
   bind(["n"], "find_under_expand", visual_mode)
   bind(["N"], "find_under_expand_skip", visual_mode)
 
-  bind(["g", "d"], "goto_definition", command_mode)
-  bind(["g", "D"], "goto_reference", command_mode)
+  bind(["g", "d"], "goto_definition", normal_mode)
+  bind(["g", "D"], "goto_reference", normal_mode)
+  bind(["g", "l"], "goto_definition", normal_mode, side_by_side=True, clear_to_right=True)
+  bind(["g", "L"], "goto_reference", normal_mode, side_by_side=True, clear_to_right=True)
 
   # Modes
   bind(["ctrl+l"], "nv_enter_normal_mode", insert_mode[0], context['no_widget'])
-  bind(["ctrl+l"], "noop", command_mode)
+  bind(["ctrl+l"], "noop", normal_mode)
   bind(["ctrl+l"], "nv_enter_normal_mode", visual_mode)
   bind(["ctrl+l"], "then_go_back_to_normal_mode", context['multiple_selections'], exec="single_selection")
 
@@ -66,8 +76,8 @@ def keybinding(bind, **kwargs):
   bind(["ctrl+m"], "enter_key", context['panel_focus'])
 
   # Text commands
-  bind(["ctrl+k"], "swap_line_up", command_mode[0])
-  bind(["ctrl+j"], "swap_line_down", command_mode[0])
+  bind(["ctrl+k"], "swap_line_up", nv_mode_enabled)
+  bind(["ctrl+j"], "swap_line_down", nv_mode_enabled)
 
   bind(["c", "c"], "replace_selection", visual_mode)
   bind(["g", "c"], "then_go_back_to_normal_mode", visual_mode, exec="toggle_comment")
@@ -76,28 +86,28 @@ def keybinding(bind, **kwargs):
   bind(["ctrl+h"], "left_delete", context['overlay_focus'])
   bind(["ctrl+j"], "insert", insert_mode, context['auto_complete_hidden'], characters="\n")
 
-  bind(["ctrl+shift+h"], [command("move_to", to="bol", extend=True), command("left_delete")], command_mode)
+  bind(["ctrl+shift+h"], [command("move_to", to="bol", extend=True), command("left_delete")], nv_mode_enabled)
 
   bind(["s", "w"], "select_inner_word", visual_mode)
 
-  bind(["d", "w"], "delete_word", command_mode)
-  bind(["d", "i", "w"], "delete_word", command_mode, scope="inner")
+  bind(["d", "w"], "delete_word", normal_mode)
+  bind(["d", "i", "w"], "delete_word", normal_mode, scope="inner")
 
-  bind(["c", "w"], "delete_word", command_mode, replace=True, scope="word_end")
-  bind(["c", "i", "w"], "delete_word", command_mode, replace=True, scope="inner")
+  bind(["c", "w"], "delete_word", normal_mode, replace=True, scope="word_end")
+  bind(["c", "i", "w"], "delete_word", normal_mode, replace=True, scope="inner")
 
   bind(["y"], "then_go_back_to_normal_mode", visual_mode, exec="copy")
   bind(["d"], "then_go_back_to_normal_mode", visual_mode, exec="cut")
   bind(["x"], "then_go_back_to_normal_mode", visual_mode, exec="right_delete")
 
   # Moves
-  bind([leader, "e"], "move_to", command_mode, to="brackets", extend=False)
+  bind([leader, "e"], "move_to", normal_mode, to="brackets", extend=False)
   bind([leader, "e"], "move_to", visual_mode, to="brackets", extend=True)
 
-  bind([leader, "h"], "nv_move_to_first_char_in_line", command_mode, extend=False)
+  bind([leader, "h"], "nv_move_to_first_char_in_line", normal_mode, extend=False)
   bind([leader, "h"], "nv_move_to_first_char_in_line", visual_mode, extend=True)
 
-  bind([leader, "l"], "nv_move_to_last_char_in_line", command_mode, extend=False)
+  bind([leader, "l"], "nv_move_to_last_char_in_line", normal_mode, extend=False)
   bind([leader, "l"], "nv_move_to_last_char_in_line", visual_mode, extend=True)
 
   # Move between overlay options
@@ -109,39 +119,49 @@ def keybinding(bind, **kwargs):
   bind(["ctrl+j"], "move", insert_mode, context['auto_complete'], by="lines", forward=True)
 
   # Plugin: AceJump
-  bind(["f"], "ace_jump_within_line", command_mode[0])
-  bind(["F"], "ace_jump_line", command_mode[0])
-  bind(["s"], "ace_jump_word", command_mode, context['single_selection'])
-  bind(["S"], "ace_jump_char", command_mode[0])
-  bind(["V"], [command("nv_enter_visual_mode"), command("ace_jump_select")], command_mode[0])
-  bind(["D"], "ace_jump_add_cursor", command_mode)
+  bind(["f"], "ace_jump_within_line", nv_mode_enabled)
+  bind(["F"], "ace_jump_line", nv_mode_enabled)
+  bind(["s"], "ace_jump_word", normal_mode, context['single_selection'])
+  bind(["S"], "ace_jump_char", nv_mode_enabled)
+  bind(["V"], [command("nv_enter_visual_mode"), command("ace_jump_select")], nv_mode_enabled)
+  bind(["D"], "ace_jump_add_cursor", normal_mode)
 
   # Plugin: File Manager
-  bind(["c", "enter"], "show_overlay", command_mode[0], overlay="command_palette", text="File Manager ")
-  bind(["c", "p"], "show_overlay", command_mode[0], overlay="command_palette", text="File Manager Copy ")
-  bind(["c", "n"], "fm_create", command_mode[0])
-  bind(["c", "f"], "create_from_current_file", command_mode)
+  bind(["c", "enter"], "show_overlay", nv_mode_enabled, overlay="command_palette", text="File Manager ")
+  bind(["c", "p"], "show_overlay", nv_mode_enabled, overlay="command_palette", text="File Manager Copy ")
+  bind(["c", "n"], "fm_create", nv_mode_enabled)
+  bind(["c", "f"], "create_from_current_file", normal_mode)
   bind(["alt+i"], "create_from_current_file", insert_mode)
   bind(["alt+i"], "insert_input", {"key": "panel", "operand": "input"}, context['panel_focus'])
+
+  # Plugin: Case Conversion
+  bind(["c", "r", "s"], "convert_to_snake", nv_mode_enabled)
+  bind(["c", "r", "S"], "convert_to_screaming_snake", nv_mode_enabled)
+  bind(["c", "r", "c"], "convert_to_camel", nv_mode_enabled)
+  bind(["c", "r", "d"], "convert_to_dash", nv_mode_enabled)
+
+  # Plugin: Column Select
+  bind(["alt+k"], "column_select", normal_mode, by="lines", forward=False)
+  bind(["alt+j"], "column_select", normal_mode, by="lines", forward=True)
 
   # Plugin: BracketHighlighter
   bind(
     ["c", "s", "q"],
     "bh_key",
-    command_mode,
+    normal_mode,
     lines=True,
     plugin={
       "type": ["single_quote", "double_quote"],
       "command": "bh_modules.swapquotes"
     }
   )
-  bind(["c", "S"], "swap_brackets", command_mode, **{"async": True})
+  bind(["c", "S"], "swap_brackets", normal_mode, **{"async": True})
 
-  bind(["d", "i", "s"], "delete_surrounded", command_mode)
-  bind(["d", "a", "s"], "delete_surrounded", command_mode, all=True)
+  bind(["d", "i", "s"], "delete_surrounded", normal_mode)
+  bind(["d", "a", "s"], "delete_surrounded", normal_mode, all=True)
 
-  bind(["c", "i", "s"], "delete_surrounded", command_mode, replace=True)
-  bind(["c", "a", "s"], "delete_surrounded", command_mode, replace=True, all=True)
+  bind(["c", "i", "s"], "delete_surrounded", normal_mode, replace=True)
+  bind(["c", "a", "s"], "delete_surrounded", normal_mode, replace=True, all=True)
 
   cs_prefix = ["c", "s"]
   change_surroundings(bind, cs_prefix, "{", "}")
@@ -246,7 +266,6 @@ context['panel_focus'] = {
 context['auto_complete'] = {
   "key": "auto_complete_visible"
 }
-
 
 context['auto_complete_hidden'] = {
   "key": "auto_complete_visible",
