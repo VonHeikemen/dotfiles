c01be50e5c668e1b7242598f3eb0e48f199e4b77
Heiker Curiel <heikercuriel@gmail.com>
2021-07-23 10:08:37 -0400

    neovim - call a vim script to tweak the colorscheme

--- my-configs/neovim/init.lua
+++ my-configs/neovim/init.lua
@@ -6,6 +6,13 @@
 -- ripgrep  - https://github.com/BurntSushi/ripgrep
 -- minpac   - https://github.com/k-takata/minpac
 
+-- tweak colorscheme
+local env = require 'conf.env'
+
+if env.theme_tweaks then
+  vim.cmd('source ' .. env.theme_tweaks)
+end
+
 -- Basic editor options
 require 'conf.basic'
 
