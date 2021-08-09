bc1d08ff985f3bf61d0101aa235115f95e519d7b
Heiker Curiel <heikercuriel@gmail.com>
2021-08-09 07:59:15 -0400

    awesome - remove error handling code
    hear me out, it looks like the fallback config is the one that catches the errors

--- my-configs/awesome/rc.lua
+++ my-configs/awesome/rc.lua
@@ -18,40 +18,9 @@ local beautiful = require 'beautiful'
 -- Prevent naughty from hijacking the system's notification daemon
 package.loaded['naughty.dbus'] = {}
 
--- Notification library
-local naughty = require 'naughty'
-
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require 'awful.hotkeys_popup.keys'
-
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-  naughty.notify({
-    preset = naughty.config.presets.critical,
-    title = 'Oops, there were errors during startup!',
-    text = awesome.startup_errors
-  })
-end
-
--- Handle runtime errors after startup
-do
-  local in_error = false
-  awesome.connect_signal('debug::error', function (err)
-    -- Make sure we don't go into an endless error loop
-    if in_error then return end
-      in_error = true
-
-      naughty.notify({
-        preset = naughty.config.presets.critical,
-        title = 'Oops, an error happened!',
-        text = tostring(err)
-      })
-
-      in_error = false
-  end)
-end
 
 -- Variable definitions
 require 'globals'
