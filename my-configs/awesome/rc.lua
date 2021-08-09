952ca813f56a6c83b18b08a6ae02734a3fa0758c
Heiker Curiel <heikercuriel@gmail.com>
2021-08-08 23:41:43 -0400

    awesome - add comment about dependencies

--- my-configs/awesome/rc.lua
+++ my-configs/awesome/rc.lua
@@ -1,3 +1,9 @@
+-- ========================================================================== --
+-- ==                            DEPENDENCIES                              == --
+-- ========================================================================== --
+
+-- lain - https://github.com/lcpz/lain
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, 'luarocks.loader')
