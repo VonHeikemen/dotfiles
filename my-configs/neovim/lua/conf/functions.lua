f5aedbf5d68dd5695610d51df6367c2c8955bbfe
Heiker Curiel <heikercuriel@gmail.com>
2022-01-11 18:35:05 -0400

    neovim - update macro command

--- my-configs/neovim/lua/conf/functions.lua
+++ my-configs/neovim/lua/conf/functions.lua
@@ -85,21 +85,21 @@ end
 M.edit_macro = function()
   local register = 'i'
 
-  local opts = {
-    prompt = 'Macro',
-    default = vim.g.edit_macro_last or ''
-  }
+  local opts = {default = vim.g.edit_macro_last or ''}
+
+  if opts.default == '' then
+    opts.prompt = 'Create Macro'
+  else
+    opts.prompt = 'Edit Macro'
+  end
 
   vim.ui.input(opts, function(value)
     if value == nil then return end
-    last_macro = value
 
     local macro = vim.fn.escape(value, '"')
-    local ok, err = pcall(vim.cmd, string.format('let @%s="%s"', register, macro))
+    vim.cmd(string.format('let @%s="%s"', register, macro))
 
-    if not ok then vim.notify(err, vim.log.levels.ERROR) end
-
-    vim.g.edit_macro_last = macro
+    vim.g.edit_macro_last = value
   end)
 end
 
