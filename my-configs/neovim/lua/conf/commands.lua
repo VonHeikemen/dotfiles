da114708034c6eee43051867d77748715778e68e
Heiker Curiel <heikercuriel@gmail.com>
2022-03-10 21:03:25 -0400

    neovim - remove command

--- my-configs/neovim/lua/conf/commands.lua
+++ my-configs/neovim/lua/conf/commands.lua
@@ -6,5 +6,3 @@ bridge.create_excmd('TrailspaceTrim', fns.trailspace_trim)
 bridge.create_excmd('SmartBufferPicker', fns.smart_buffer_picker)
 bridge.create_excmd('EditMacro', fns.edit_macro)
 
-vim.cmd('command! -nargs=* -bang LspSetupServers lua require("lsp").setup_servers({root_dir = "<bang>" == "!", <f-args>})')
-
