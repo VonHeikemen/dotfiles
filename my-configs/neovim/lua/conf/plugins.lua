8a2d85f8a95750cf612deae83d32760cab72293e
Heiker Curiel <heikercuriel@gmail.com>
2022-03-17 21:01:19 -0400

    neovim - update luasnip settings

--- my-configs/neovim/lua/conf/plugins.lua
+++ my-configs/neovim/lua/conf/plugins.lua
@@ -102,11 +102,17 @@ plug.init({
       local snippets = require('luasnip.loaders.from_vscode')
 
       luasnip.config.set_config({
+        history = false,
         region_check_events = 'InsertEnter',
         delete_check_events = 'InsertLeave'
       })
 
-      snippets.load()
+      snippets.lazy_load()
+      local filetype = vim.bo.filetype
+
+      if vim.fn.argc() > 0 and filetype ~= '' then
+        snippets.load({include = {filetype}})
+      end
     end
   },
   {
