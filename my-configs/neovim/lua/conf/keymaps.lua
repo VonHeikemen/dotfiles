bc9cb087d29450b6e34d1008e6ca6c782db5b17c
Heiker Curiel <heikercuriel@gmail.com>
2022-04-25 10:02:50 -0400

    neovim - update keymaps

--- my-configs/neovim/lua/conf/keymaps.lua
+++ my-configs/neovim/lua/conf/keymaps.lua
@@ -2,11 +2,10 @@ local fns = require('conf.functions')
 
 local augroup = vim.api.nvim_create_augroup('mapping_cmds', {clear = true})
 local autocmd = vim.api.nvim_create_autocmd
-local bind = vim.keymap.set
 
 -- Bind options
-local remap = function(m, lhs, rhs) bind(m, lhs, rhs, {remap = true}) end
-local noremap = function(m, lhs, rhs) bind(m, lhs, rhs, {remap = false}) end
+local bind = vim.keymap.set
+local remap = {remap = true}
 
 -- Leader
 vim.g.mapleader = ' '
@@ -16,153 +15,153 @@ vim.g.mapleader = ' '
 -- ========================================================================== --
 
 -- Enter commands
-noremap('n', '<CR>', ':FineCmdline<CR>')
+bind('n', '<CR>', ':FineCmdline<CR>')
 
 -- Select all text in current buffer
-noremap('n', '<Leader>a', ':keepjumps normal! ggVG<CR>')
+bind('n', '<Leader>a', ':keepjumps normal! ggVG<CR>')
 
 -- Go to matching pair
-remap({'n', 'x'}, '<Leader>e', '%')
+bind({'n', 'x'}, '<Leader>e', '%', remap)
 
 -- Go to first character in line
-noremap('', '<Leader>h', '^')
+bind('', '<Leader>h', '^')
 
 -- Go to last character in line
-noremap('', '<Leader>l', 'g_')
+bind('', '<Leader>l', 'g_')
 
 -- Scroll half page and center
-noremap('', '<C-u>', '<C-u>M')
-noremap('', '<C-d>', '<C-d>M')
+bind('', '<C-u>', '<C-u>M')
+bind('', '<C-d>', '<C-d>M')
 
 -- Search will center on the line it's found in
-noremap('n', 'n', 'nzzzv')
-noremap('n', 'N', 'Nzzzv')
-noremap('n', '#', '#zz')
-noremap('n', '*', '*zz')
+bind('n', 'n', 'nzzzv')
+bind('n', 'N', 'Nzzzv')
+bind('n', '#', '#zz')
+bind('n', '*', '*zz')
 
 -- Delete in select mode
-noremap('s', '<BS>', '<C-g>"_c')
-remap('s', '<C-h>', '<BS>')
+bind('s', '<BS>', '<C-g>"_c')
+bind('s', '<C-h>', '<BS>', remap)
 
 -- Because of reasons
-remap('i', '<C-h>', '<BS>')
+bind('i', '<C-h>', '<BS>', remap)
 
 -- Whatever you delete, make it go away
-noremap('n', 'c','"_c')
-noremap('n', 'C','"_C')
-noremap('x', 'c','"_c')
-noremap('x', 'C','"_C')
+bind('n', 'c','"_c')
+bind('n', 'C','"_C')
+bind('x', 'c','"_c')
+bind('x', 'C','"_C')
 
-noremap('n', 'x','"_x')
-noremap('x', 'x','"_x')
-noremap('x', 'X','"_c')
+bind('n', 'x','"_x')
+bind('x', 'x','"_x')
+bind('x', 'X','"_c')
 
 -- ========================================================================== --
 -- ==                           COMMAND MAPPINGS                           == --
 -- ========================================================================== --
 
 -- Moving lines and preserving indentation
-noremap('n', '<C-j>', ":move .+1<CR>==")
-noremap('n', '<C-k>', ":move .-2<CR>==")
-noremap('v', '<C-j>', ":move '>+1<CR>gv=gv")
-noremap('v', '<C-k>', ":move '<-2<CR>gv=gv")
+bind('n', '<C-j>', ":move .+1<CR>==")
+bind('n', '<C-k>', ":move .-2<CR>==")
+bind('v', '<C-j>', ":move '>+1<CR>gv=gv")
+bind('v', '<C-k>', ":move '<-2<CR>gv=gv")
 
 -- Write file
-noremap('n', '<Leader>w', ':write<CR>')
+bind('n', '<Leader>w', ':write<CR>')
 
 -- Safe quit
-noremap('n', '<Leader>qq', ':quitall<CR>')
+bind('n', '<Leader>qq', ':quitall<CR>')
 
 -- Force quit
-noremap('n', '<Leader>Q', ':quitall!<CR>')
+bind('n', '<Leader>Q', ':quitall!<CR>')
 
 -- Close buffer
-noremap('n', '<Leader>bq', ':bdelete<CR>')
+bind('n', '<Leader>bq', ':bdelete<CR>')
 
 -- Move to last active buffer
-noremap('n', '<Leader>bl', ':buffer #<CR>')
+bind('n', '<Leader>bl', ':buffer #<CR>')
 
 -- Navigate between buffers
-noremap('n', '[b', ':bprevious<CR>')
-noremap('n', ']b', ':bnext<CR>')
+bind('n', '[b', ':bprevious<CR>')
+bind('n', ']b', ':bnext<CR>')
 
 -- Open new tabpage
-noremap('n', '<Leader>tn', ':tabnew<CR>')
+bind('n', '<Leader>tn', ':tabnew<CR>')
 
 -- Navigate between tabpages
-noremap('n', '[t', ':tabprevious<CR>')
-noremap('n', ']t', ':tabnext<CR>')
+bind('n', '[t', ':tabprevious<CR>')
+bind('n', ']t', ':tabnext<CR>')
 
 -- Clear messages
-noremap('n', '<Leader><space>', ":echo ''<CR>")
+bind('n', '<Leader><space>', ":echo ''<CR>")
 
 -- Switch to the directory of the open buffer
-noremap('n', '<Leader>cd', ':lcd %:p:h<CR>:pwd<CR>')
+bind('n', '<Leader>cd', ':lcd %:p:h<CR>:pwd<CR>')
 
 -- ========================================================================== --
 -- ==                           TOGGLE ELEMENTS                            == --
 -- ========================================================================== --
 
 -- Search result highlight
-noremap('n', '<Leader>uh', '<cmd>set invhlsearch<CR>')
+bind('n', '<Leader>uh', '<cmd>set invhlsearch<CR>')
 
 -- Tabline
-noremap('n', '<Leader>ut', fns.toggle_opt('showtabline', 'o', 1, 0))
+bind('n', '<Leader>ut', fns.toggle_opt('showtabline', 'o', 1, 0))
 
 -- Line length ruler
-noremap('n', '<Leader>ul', fns.toggle_opt('colorcolumn', 'wo', '81', '0'))
+bind('n', '<Leader>ul', fns.toggle_opt('colorcolumn', 'wo', '81', '0'))
 
 -- Cursorline highlight
-noremap('n', '<Leader>uc', '<cmd>set invcursorline<CR>')
+bind('n', '<Leader>uc', '<cmd>set invcursorline<CR>')
 
 -- Line numbers
-noremap('n', '<Leader>un', '<cmd>set invnumber<CR>')
+bind('n', '<Leader>un', '<cmd>set invnumber<CR>')
 
 -- Relative line numbers
-noremap('n', '<Leader>ur', '<cmd>set invrelativenumber<CR>')
+bind('n', '<Leader>ur', '<cmd>set invrelativenumber<CR>')
 
 -- ========================================================================== --
 -- ==                           SEARCH COMMANDS                            == --
 -- ========================================================================== --
 
 -- Show key bindings list
-noremap('n', '<Leader>?', ':Telescope keymaps<CR>')
+bind('n', '<Leader>?', ':Telescope keymaps<CR>')
 
 -- Search pattern
-noremap('n', '<leader>F', ':FineCmdline TGrep <CR>')
-noremap('x', '<Leader>F', ':<C-u>GetSelection<CR>:TGrep <C-r>/<CR>')
-noremap('n', '<Leader>fw', ":TGrep <C-r>=expand('<cword>')<CR><CR>")
-noremap('n', '<leader>fg', ':Telescope live_grep<CR>')
+bind('n', '<leader>F', ':FineCmdline TGrep <CR>')
+bind('x', '<Leader>F', ':<C-u>GetSelection<CR>:TGrep <C-r>/<CR>')
+bind('n', '<Leader>fw', ":TGrep <C-r>=expand('<cword>')<CR><CR>")
+bind('n', '<leader>fg', ':Telescope live_grep<CR>')
 
 -- Find files by name
-noremap('n', '<Leader>ff', ':Telescope find_files<CR>')
+bind('n', '<Leader>ff', ':Telescope find_files<CR>')
 
 -- Search symbols in buffer
-noremap('n', '<Leader>fs', ':Telescope treesitter<CR>')
+bind('n', '<Leader>fs', ':Telescope treesitter<CR>')
 
 -- Search in files history
-noremap('n', '<Leader>fh', ':Telescope oldfiles<CR>')
+bind('n', '<Leader>fh', ':Telescope oldfiles<CR>')
 
 -- Search in active buffers list
-noremap('n', '<Leader>bb', ':Telescope buffers<CR>')
-noremap('n', '<Leader>B', ':Telescope buffers only_cwd=true<CR>')
+bind('n', '<Leader>bb', ':Telescope buffers<CR>')
+bind('n', '<Leader>B', ':Telescope buffers only_cwd=true<CR>')
 
 -- Put selected text in register '/'
-noremap('v', '<Leader>y', ':<C-u>GetSelection<CR>gv')
-noremap('v', '<Leader>Y', ':<C-u>GetSelection<CR>:set hlsearch<CR>')
+bind('v', '<Leader>y', ':<C-u>GetSelection<CR>gv')
+bind('v', '<Leader>Y', ':<C-u>GetSelection<CR>:set hlsearch<CR>')
 
 -- Nice buffer local search
-noremap('n', '<leader>s', ':SearchBoxIncSearch<CR>')
-noremap('x', '<leader>s', ':SearchBoxIncSearch visual_mode=true<CR>')
-noremap('n', '<leader>S', ":SearchBoxMatchAll title=' Match '<CR>")
-noremap('x', '<leader>S', ":SearchBoxMatchAll title=' Match ' visual_mode=true<CR>")
-noremap('n', '<leader>;', '<cmd>SearchBoxClear<CR>')
+bind('n', '<leader>s', ':SearchBoxIncSearch<CR>')
+bind('x', '<leader>s', ':SearchBoxIncSearch visual_mode=true<CR>')
+bind('n', '<leader>S', ":SearchBoxMatchAll title=' Match '<CR>")
+bind('x', '<leader>S', ":SearchBoxMatchAll title=' Match ' visual_mode=true<CR>")
+bind('n', '<leader>;', '<cmd>SearchBoxClear<CR>')
 
 -- Begin search & replace
-noremap('n', '<leader>r', ":SearchBoxReplace confirm=menu<CR>")
-noremap('x', '<leader>r', ":SearchBoxReplace confirm=menu visual_mode=true<CR>")
-noremap('n', '<leader>R', ":SearchBoxReplace confirm=menu -- <C-r>=expand('<cword>')<CR><CR>")
-noremap('x', '<leader>R', ":<C-u>GetSelection<CR>:SearchBoxReplace confirm=menu<CR><C-r>/")
+bind('n', '<leader>r', ":SearchBoxReplace confirm=menu<CR>")
+bind('x', '<leader>r', ":SearchBoxReplace confirm=menu visual_mode=true<CR>")
+bind('n', '<leader>R', ":SearchBoxReplace confirm=menu -- <C-r>=expand('<cword>')<CR><CR>")
+bind('x', '<leader>R', ":<C-u>GetSelection<CR>:SearchBoxReplace confirm=menu<CR><C-r>/")
 
 -- ========================================================================== --
 -- ==                                 LSP                                  == --
@@ -207,29 +206,29 @@ autocmd('User', {
 -- ========================================================================== --
 
 -- Change the whole word
-noremap('n', 'cw', '"_ciw')
+bind('n', 'cw', '"_ciw')
 
 -- Change word but repeatable
-noremap('n', '<leader>cw', "<cmd>let @/='\\<'.expand('<cword>').'\\>'<cr>\"_ciw")
+bind('n', '<leader>cw', "<cmd>let @/='\\<'.expand('<cword>').'\\>'<cr>\"_ciw")
 
 -- Use lightspeed's omni mode
-remap('', 's', '<Plug>Lightspeed_omni_s')
+bind('', 's', '<Plug>Lightspeed_omni_s')
 
 -- Close buffer while preserving the layout
-noremap('n', '<Leader>bc', ':Bdelete<CR>')
+bind('n', '<Leader>bc', ':Bdelete<CR>')
 
 -- Toggle zen-mode
-noremap('n', '<Leader>uz', '<cmd>ZenMode<CR>')
+bind('n', '<Leader>uz', '<cmd>ZenMode<CR>')
 
 -- Override some `cv` bindings from `vim-system-copy`.
-remap('n', 'cvv', 'ax<Esc><plug>SystemPastel')
-remap('n', 'cvk', 'Ox<Esc><Plug>SystemPastel')
-remap('n', 'cvj', 'ox<Esc><Plug>SystemPastel')
+bind('n', 'cvv', 'ax<Esc><plug>SystemPastel', remap)
+bind('n', 'cvk', 'Ox<Esc><Plug>SystemPastel', remap)
+bind('n', 'cvj', 'ox<Esc><Plug>SystemPastel', remap)
 
 -- Manage the quickfix list
-remap('n', '[q', '<Plug>(qf_qf_previous)zz')
-remap('n', ']q', '<Plug>(qf_qf_next)zz')
-remap('n', '<Leader>cc', '<Plug>(qf_qf_toggle)')
+bind('n', '[q', '<Plug>(qf_qf_previous)zz')
+bind('n', ']q', '<Plug>(qf_qf_next)zz')
+bind('n', '<Leader>cc', '<Plug>(qf_qf_toggle)')
 
 autocmd('filetype', {
   pattern = 'qf',
@@ -251,12 +250,12 @@ autocmd('filetype', {
 })
 
 -- Open file manager
-noremap('n', '<leader>dd', fns.file_explorer)
-noremap('n', '<leader>da', function() fns.file_explorer(vim.fn.getcwd()) end)
+bind('n', '<leader>dd', fns.file_explorer)
+bind('n', '<leader>da', function() fns.file_explorer(vim.fn.getcwd()) end)
 
 -- Undo break points
 local break_points = {'<Space>', '-', '_', ':', '.', '/'}
 for _, char in ipairs(break_points) do
-  noremap('i', char, char .. '<C-g>u')
+  bind('i', char, char .. '<C-g>u')
 end
 
