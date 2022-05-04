ea6965270f17118cfa6910eea4a7a03df20306c4
Heiker Curiel <heikercuriel@gmail.com>
2022-05-04 08:25:38 -0400

    neovim - change minimum log for nvim-notify

--- my-configs/neovim/lua/plugins/misc.lua
+++ my-configs/neovim/lua/plugins/misc.lua
@@ -42,6 +42,7 @@ M.nvim_notify = function()
   vim.notify = require('notify')
   vim.notify.setup({
     stages = 'slide',
+    level = 'DEBUG',
     background_colour = vim.g.terminal_color_background,
     minimum_width = 15
   })
