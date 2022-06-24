8799c417f41fdce4035583a49403d332eba67799
Heiker Curiel <heikercuriel@gmail.com>
2022-06-23 20:31:17 -0400

    neovim - add eslint lsp config

---
+++ my-configs/neovim/lua/lsp/configs/eslint.lua
@@ -0,0 +1,66 @@
+local shared = require('lsp.configs.shared')
+local cwd = vim.fn.getcwd()
+
+local filetypes = {
+  'javascript',
+  'javascriptreact',
+  'javascript.jsx',
+  'typescript',
+  'typescriptreact',
+  'typescript.tsx',
+  'vue'
+}
+
+local server = shared.make_config({
+  cmd = {'vscode-eslint-language-server', '--stdio'},
+  name = 'eslint',
+  filetypes = filetypes,
+  settings = {
+    -- required
+    validate = 'on',
+    rulesCustomizations = {},
+    nodePath = '',
+    workspaceFolder = {
+      uri = vim.uri_from_fname(cwd),
+      name = cwd,
+    },
+
+    -- optional
+    packageManager = 'npm',
+    useESLintClass = false,
+    codeActionOnSave = {
+      enable = false,
+      mode = 'all',
+    },
+    format = false,
+    quiet = false,
+    onIgnoredFiles = 'off',
+    run = 'onType',
+    workingDirectory = {mode = 'location'},
+    codeAction = {
+      disableRuleComment = {
+        enable = true,
+        location = 'separateLine',
+      },
+    },
+  },
+  handlers = {
+    ['eslint/confirmESLintExecution'] = function(_, result)
+      if not result then
+        return
+      end
+      return 4 -- approved
+    end,
+    ['eslint/probeFailed'] = function()
+      vim.notify('[lsp] ESLint probe failed.', vim.log.levels.WARN)
+      return {}
+    end,
+    ['eslint/noLibrary'] = function()
+      vim.notify('[lsp] Unable to find ESLint library.', vim.log.levels.WARN)
+      return {}
+    end,
+  }
+})
+
+return server
+
