4f08914032d2cdee35175735e1432065964a485b
Heiker Curiel <heikercuriel@gmail.com>
2022-07-08 21:58:38 -0400

    neovim - fix default highlight in statusline

--- my-configs/neovim/lua/plugins/statusline.lua
+++ my-configs/neovim/lua/plugins/statusline.lua
@@ -2,8 +2,9 @@ local M = {}
 local state = {}
 
 local default_hl = function(name, style)
-  local ok, hl = pcall(vim.api.nvim_get_hl_by_name, name, 1)
-  if hl == 0 then return end
+  local ok, _ = pcall(vim.api.nvim_get_hl_by_name, name, 1)
+  if ok then return end
+
   vim.api.nvim_set_hl(0, name, style)
 end
 
