508852ce03584cd811937ac9993f3e73221f22e6
Heiker Curiel <heikercuriel@gmail.com>
2022-07-19 19:47:18 -0400

    neovim - update searchbox config

--- my-configs/neovim/lua/plugins/searchbox.lua
+++ my-configs/neovim/lua/plugins/searchbox.lua
@@ -1,9 +1,7 @@
 require('searchbox').setup({
-  hooks = {
-    on_done = function(value)
-      if value == nil then return end
-      vim.fn.setreg('s', value)
-    end
+  defaults = {
+    modifier = 'plain',
+    confirm = 'menu',
   }
 })
 
