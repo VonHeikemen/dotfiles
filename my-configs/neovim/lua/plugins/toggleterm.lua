fdcd7f78ffdc86d9d78ea75098c6b357e5ff1327
Heiker Curiel <heikercuriel@gmail.com>
2022-09-10 09:37:15 -0400

    neovim - update toggleterm config

--- my-configs/neovim/lua/plugins/toggleterm.lua
+++ my-configs/neovim/lua/plugins/toggleterm.lua
@@ -8,21 +8,26 @@ require('toggleterm').setup({
 local open_mapping = '<M-i>'
 
 local function toggle()
-  local percent = 0.4
   local term = require('toggleterm') 
 
   if vim.o.lines < 17 then
-    local size = vim.o.columns * percent
+    local size = vim.o.columns * 0.4
     term.toggle(vim.v.count, size, nil, 'vertical')
     return
   end
 
-  local size = vim.o.lines * percent
+  local size = vim.o.lines * 0.3
   term.toggle(vim.v.count, size, nil, 'horizontal')
 end
 
 vim.keymap.set({'n', 'i', 'x'}, open_mapping, toggle, {desc = 'Toggle terminal'})
 vim.keymap.set('t', open_mapping, '<cmd>ToggleTerm<cr>')
+
+vim.keymap.set('n', '<C-w>t', '<cmd>ToggleTerm direction=tab<cr>')
+vim.keymap.set('n', '<C-w>f', '<cmd>ToggleTerm direction=float<cr>')
+vim.keymap.set('t', '<C-w>t', '<cmd>ToggleTerm<cr><cmd>ToggleTerm direction=tab<cr>')
+vim.keymap.set('t', '<C-w>f', '<cmd>ToggleTerm<cr><cmd>ToggleTerm direction=float<cr>')
+
 vim.keymap.set('t', '<C-w>w', '<C-\\><C-n><C-w>w')
 vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h')
 vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k')
