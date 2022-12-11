a6a831e911591015eccdefbc2aebf4b22b5b1267
Heiker Curiel <heikercuriel@gmail.com>
2022-12-10 22:10:50 -0400

    neovim - fix init

--- my-configs/neovim/init.lua
+++ my-configs/neovim/init.lua
@@ -13,7 +13,7 @@ local ok, env = pcall(require, 'user.env')
 
 if not ok then
   local msg = 'lua/user/env.lua not found. You should probably rename env.sample',
-  vim.notify(msg vim.log.levels.ERROR)
+  vim.notify(msg, vim.log.levels.ERROR)
   return
 end
 
