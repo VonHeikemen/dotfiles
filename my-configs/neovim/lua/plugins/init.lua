dbec52ea6be1d7110c0dc8d650118d6be1ad87cb
Heiker Curiel <heikercuriel@gmail.com>
2023-04-08 16:27:28 -0400

    neovim - update plugins lock file

--- my-configs/neovim/lua/plugins/init.lua
+++ my-configs/neovim/lua/plugins/init.lua
@@ -1,18 +1,9 @@
 local Plugins = {
-  {'editorconfig/editorconfig-vim', lazy = true},
   {'mattn/emmet-vim', enabled = false},
 
   -- Utilities
   {'tpope/vim-repeat', keys = '.'},
   {'tpope/vim-abolish', keys = 'cr'},
-  {
-    'nvim-treesitter/playground',
-    cmd = {
-      'TSPlaygroundToggle',
-      'TSNodeUnderCursor',
-      'TSHighlightCapturesUnderCursor'
-    }
-  },
 }
 
 return Plugins
