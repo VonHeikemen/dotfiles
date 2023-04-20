38aca2151de9e12b1cc6e99d21dad567553ca05d
Heiker Curiel <heikercuriel@gmail.com>
2023-04-20 09:32:48 -0400

    zsh - add env vars to simple.zsh

--- my-configs/zsh/simple.zsh
+++ my-configs/zsh/simple.zsh
@@ -149,3 +149,13 @@ alias rmf='rm -rf'
 
 alias bb='echo -ne "\e[6 q"'
 
+###
+## Environment
+###
+
+export PATH=/usr/local/bin:/usr/bin:/bin:$HOME/.local/bin
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.cache"
+
