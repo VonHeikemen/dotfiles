075673733968ff18916f629c8701c1f782e0bf1e
Heiker Curiel <heikercuriel@gmail.com>
2023-09-28 09:47:01 -0400

    neovim - update searchbox config

--- my-configs/neovim/lua/plugins/searchbox.lua
+++ my-configs/neovim/lua/plugins/searchbox.lua
@@ -3,14 +3,12 @@ local Plugin = {'VonHeikemen/searchbox.nvim'}
 
 Plugin.dependencies = {{'MunifTanjim/nui.nvim'}}
 
-Plugin.name = 'searchbox'
-
 Plugin.opts = {
   defaults = {
     modifier = 'plain',
     confirm = 'native',
     show_matches = '[T:{total}]'
-  }
+  },
 }
 
 Plugin.cmd = {
@@ -22,14 +20,14 @@ Plugin.cmd = {
 function Plugin.init()
   local bind = vim.keymap.set
 
+  bind('n', 's', '<cmd>SearchBoxIncSearch<cr>')
   bind('n', 'si', '<cmd>SearchBoxIncSearch<cr>')
   bind('x', 'si', "<Esc><cmd>'<,'>SearchBoxIncSearch visual_mode=true<cr>")
-  bind('n', 'sj', "<cmd>SearchBoxMatchAll title=' Match '<cr>")
-  bind('x', 'sj', "<Esc><cmd>'<,'>SearchBoxMatchAll title=' Match ' visual_mode=true<cr>")
-  bind('n', '<leader>s', '<cmd>SearchBoxClear<cr>')
+  bind('n', 'sj', "<cmd>exe 'SearchBoxIncSearch  --' expand('<cword>')<cr>")
+  bind('x', 'sj', "<Esc><cmd>GetSelection<cr><cmd>exe 'SearchBoxIncSearch --' getreg('/')<cr>")
 
-  bind('n', 'r', "<cmd>SearchBoxReplace <CR>")
-  bind('x', 'r', "<Esc><cmd>SearchBoxReplace  visual_mode=true<cr>")
+  bind('n', 'r', '<cmd>SearchBoxReplace <CR>')
+  bind('x', 'r', '<Esc><cmd>SearchBoxReplace  visual_mode=true<cr>')
   bind('n', 'R', "<cmd>exe 'SearchBoxReplace  --' expand('<cword>')<cr>")
   bind('x', 'R', "<Esc><cmd>GetSelection<cr><cmd>exe 'SearchBoxReplace --' getreg('/')<cr>")
 end
