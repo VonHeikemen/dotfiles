4d63fb00d4e67ffcb655542a503eb3a902a5ebbb
Heiker Curiel <heikercuriel@gmail.com>
2023-09-28 09:47:10 -0400

    neovim - update indent-blankline

--- my-configs/neovim/lua/plugins/indent-blankline.lua
+++ my-configs/neovim/lua/plugins/indent-blankline.lua
@@ -1,21 +1,24 @@
 -- Indent guides
 local Plugin = {'lukas-reineke/indent-blankline.nvim'}
 
-Plugin.name = 'indent_blankline'
+Plugin.version = '3.x'
+
+Plugin.main = 'ibl'
 
 Plugin.event = {'BufReadPost', 'BufNewFile'}
 
 Plugin.opts = {
   enabled = false,
-  char = '▏',
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = false
+  scope = {
+    enabled = false,
+  },
+  indent = {
+    char = '▏',
+  },
 }
 
 function Plugin.init()
-  vim.keymap.set('n', '<leader>ui', '<cmd>IndentBlanklineToggle<cr>')
+  vim.keymap.set('n', '<leader>ui', '<cmd>IBLToggle<cr>')
 end
 
 return Plugin
