ae32fcda54ba94fa535ac55e4e3743beb248c799
Heiker Curiel <heikercuriel@gmail.com>
2024-01-24 20:59:15 -0400

    neovim - move local plugin setup

--- my-configs/neovim/lua/user/plugins.lua
+++ my-configs/neovim/lua/user/plugins.lua
@@ -45,13 +45,3 @@ lazy.setup({
   {import = 'plugins'}
 })
 
--- ========================================================================== --
--- ==                            LOCAL PLUGINS                             == --
--- ========================================================================== --
-
-require('local.tmux')
-require('local.session')
-require('local.tabline').setup()
-require('local.buffer-nav').setup()
-require('local.statusline').setup()
-
---
+++ my-configs/neovim/plugin/homemade.lua
@@ -0,0 +1,10 @@
+-- ========================================================================== --
+-- ==                            LOCAL PLUGINS                             == --
+-- ========================================================================== --
+
+require('local.tmux')
+require('local.session')
+require('local.tabline').setup()
+require('local.buffer-nav').setup()
+require('local.statusline').setup()
+
