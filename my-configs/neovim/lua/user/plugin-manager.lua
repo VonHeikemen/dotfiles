538fa441dff8a38e6e9f49e6dedddd7ced189761
Heiker Curiel <heikercuriel@gmail.com>
2024-01-24 21:10:47 -0400

    neovim - autocommand to backup lazy lock file

--- my-configs/neovim/lua/user/plugin-manager.lua
+++ my-configs/neovim/lua/user/plugin-manager.lua
@@ -45,3 +45,24 @@ lazy.setup({
   {import = 'specs'}
 })
 
+local augroup = vim.api.nvim_create_augroup('lazy_cmds', {clear = true})
+local snapshot_dir = vim.fn.stdpath('data') .. '/plugin-snapshot'
+local lockfile = vim.fn.stdpath('config') .. '/lazy-lock.json'
+
+vim.api.nvim_create_user_command('LazySnapshot', function()
+  vim.cmd.edit(snapshot_dir)
+  vim.cmd.vsplit(lockfile)
+end, {})
+
+vim.api.nvim_create_autocmd('User', {
+  group = augroup,
+  pattern = 'LazyUpdatePre',
+  desc = 'Backup lazy.nvim lockfile',
+  callback = function()
+    vim.fn.mkdir(snapshot_dir, 'p')
+    local snapshot = snapshot_dir .. os.date('/%Y-%m-%dT%H:%M:%S.json')
+
+    vim.loop.fs_copyfile(lockfile, snapshot)
+  end,
+})
+
