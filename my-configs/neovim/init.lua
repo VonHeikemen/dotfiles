f1811e2e58f7ffb0bfdd5b9119b0779c014eafe7
Heiker Curiel <heikercuriel@gmail.com>
2024-06-24 07:56:44 -0400

    neovim - update dependencies comment

--- my-configs/neovim/init.lua
+++ my-configs/neovim/init.lua
@@ -5,7 +5,6 @@
 -- ripgrep    - https://github.com/BurntSushi/ripgrep
 -- fd         - https://github.com/sharkdp/fd
 -- git        - https://git-scm.com/
--- make       - https://www.gnu.org/software/make/
 -- c compiler - gcc or tcc or zig
 
 -- Try to load "env" file
