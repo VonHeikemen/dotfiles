f64d1eeee3dca6680a4d7b03f69e669cc07ef292
Heiker Curiel <heikercuriel@gmail.com>
2024-07-02 00:10:13 -0400

    neovim - update lsp config

--- my-configs/neovim/lua/specs/lsp.lua
+++ my-configs/neovim/lua/specs/lsp.lua
@@ -13,24 +13,11 @@ Plugin.cmd = 'Lsp'
 function Plugin.init()
   vim.g.lsp_zero_extend_cmp = 0
   vim.g.lsp_zero_extend_lspconfig = 0
-
-  -- disable lsp semantic highlights
-  vim.api.nvim_create_autocmd('ColorScheme', {
-    desc = 'Clear LSP highlight groups',
-    callback = function()
-      local higroups = vim.fn.getcompletion('@lsp', 'highlight')
-      for _, name in ipairs(higroups) do
-        vim.api.nvim_set_hl(0, name, {})
-      end
-    end,
-  })
 end
 
 function Plugin.config()
-  local lz = require('lsp-zero')
-
-  user.lspconfig(lz)
-  user.diagnostics(lz)
+  user.lspconfig()
+  user.diagnostics()
 
   require('mason-lspconfig').setup({})
 
@@ -41,13 +28,15 @@ function Plugin.config()
         return
       end
 
-      lz.use(input.args, {})
+      require('lsp-zero').use(input.args, {})
     end,
     {desc = 'Initialize a language server', nargs = '?'}
   )
 end
 
-function user.lspconfig(lsp)
+function user.lspconfig()
+  local lsp = require('lsp-zero')
+
   lsp.extend_lspconfig()
 
   lsp.on_attach(user.lsp_attach)
@@ -77,13 +66,15 @@ function user.lspconfig(lsp)
   }
 end
 
-function user.lsp_attach(_, bufnr)
+function user.lsp_attach(client, bufnr)
   local telescope = require('telescope.builtin')
-  local lsp = vim.lsp.buf
   local bind = vim.keymap.set
   local command = vim.api.nvim_buf_create_user_command
 
-  command(0, 'LspFormat', function(input)
+  -- Disable semantic highlights
+  client.server_capabilities.semanticTokensProvider = nil
+
+  command(bufnr, 'LspFormat', function(input)
     vim.lsp.buf.format({async = input.bang})
   end, {})
 
@@ -91,37 +82,39 @@ function user.lsp_attach(_, bufnr)
 
   bind({'n', 'x'}, 'gq', '<cmd>LspFormat!<cr>', opts)
 
-  bind('n', 'K', lsp.hover, opts)
-  bind('n', 'gd', lsp.definition, opts)
-  bind('n', 'gD', lsp.declaration, opts)
-  bind('n', 'gi', lsp.implementation, opts)
-  bind('n', 'go', lsp.type_definition, opts)
-  bind('n', 'gr', lsp.references, opts)
-  bind('n', 'gs', lsp.signature_help, opts)
-  bind('n', '<F2>', lsp.rename, opts)
-  bind('n', '<F4>', lsp.code_action, opts)
+  bind('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  bind('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  bind('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  bind('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  bind('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  bind('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  bind('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  bind('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  bind('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
-  bind('n', 'gl', vim.diagnostic.open_float, opts)
-  bind('n', '[d', vim.diagnostic.goto_prev, opts)
-  bind('n', ']d', vim.diagnostic.goto_next, opts)
+  bind('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+  bind('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+  bind('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
 
   bind('n', '<leader>fd', telescope.lsp_document_symbols, opts)
   bind('n', '<leader>fq', telescope.lsp_workspace_symbols, opts)
 end
 
-function user.diagnostics(lsp)
+function user.diagnostics()
   local augroup = vim.api.nvim_create_augroup
   local autocmd = vim.api.nvim_create_autocmd
-
-  lsp.set_sign_icons({
-    error = '✘',
-    warn = '▲',
-    hint = '⚑',
-    info = '»'
-  })
+  local level = vim.diagnostic.severity
 
   vim.diagnostic.config({
     virtual_text = false,
+    signs = {
+      text = {
+        [level.ERROR] = '✘',
+        [level.WARN] = '▲',
+        [level.HINT] = '⚑',
+        [level.INFO] = '»',
+      }
+    }
   })
 
   local group = augroup('diagnostic_cmds', {clear = true})
@@ -130,14 +123,14 @@ function user.diagnostics(lsp)
     group = group,
     pattern = {'n:i', 'v:s'},
     desc = 'Disable diagnostics while typing',
-    callback = function(e) vim.diagnostic.disable(e.buf) end
+    callback = function(e) vim.diagnostic.enable(false, {bufnr = e.buf}) end
   })
 
   autocmd('ModeChanged', {
     group = group,
     pattern = 'i:n',
     desc = 'Enable diagnostics when leaving insert mode',
-    callback = function(e) vim.diagnostic.enable(e.buf) end
+    callback = function(e) vim.diagnostic.enable(true, {bufnr = e.buf}) end
   })
 end
 
