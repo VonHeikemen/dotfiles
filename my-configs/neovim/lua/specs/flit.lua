fcf54fe91911ac8af4c577b80f92e20171ce4549
Heiker Curiel <heikercuriel@gmail.com>
2024-08-20 09:35:33 -0400

    neovim - enable flit

--- my-configs/neovim/lua/specs/flit.lua
+++ my-configs/neovim/lua/specs/flit.lua
@@ -1,21 +1,14 @@
----
--- Plugin is disabled for now, because I disabled leap.nvim
----
-
 -- Enhanced f/t moves
 local Plugin = {'ggandor/flit.nvim'}
 
-Plugin.enabled = false
+Plugin.lazy = false
+
+Plugin.dependencies = {
+  {'ggandor/leap.nvim'},
+}
 
 Plugin.opts = {
   multiline = false,
-}
-
-Plugin.keys = {
-  {'f', mode = {'n', 'x', 'o'}},
-  {'F', mode = {'n', 'x', 'o'}},
-  {'t', mode = {'n', 'x', 'o'}},
-  {'T', mode = {'n', 'x', 'o'}},
 }
 
 return Plugin
