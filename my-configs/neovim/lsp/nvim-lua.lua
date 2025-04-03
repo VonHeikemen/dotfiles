33e4db09294c238a83dd19b99e7a84fb551858ca
Heiker Curiel <heikercuriel@gmail.com>
2025-04-02 20:36:30 -0400

    neovim - use default runtime.path

--- my-configs/neovim/lsp/nvim-lua.lua
+++ my-configs/neovim/lsp/nvim-lua.lua
@@ -1,13 +1,7 @@
-local join = vim.fs.joinpath
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, join('lua', '?.lua'))
-table.insert(runtime_path, join('lua', '?', 'init.lua'))
-
 local nvim_settings = {
   runtime = {
     -- Tell the language server which version of Lua you're using
     version = 'LuaJIT',
-    path = runtime_path,
   },
   diagnostics = {
     -- Get the language server to recognize the `vim` global
