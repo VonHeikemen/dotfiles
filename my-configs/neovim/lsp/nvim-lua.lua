3a998dd22a00f8c59a82fbe52d6fa13960cc8a33
Heiker Curiel <heikercuriel@gmail.com>
2025-04-14 09:19:55 -0400

    neovim - update nvim-lua settings

--- my-configs/neovim/lsp/nvim-lua.lua
+++ my-configs/neovim/lsp/nvim-lua.lua
@@ -1,7 +1,14 @@
+-- type definitions for vim.uv:
+-- https://github.com/Bilal2453/luvit-meta
+
 local nvim_settings = {
   runtime = {
     -- Tell the language server which version of Lua you're using
     version = 'LuaJIT',
+    path = {
+      'lua/?.lua',
+      'lua/?/init.lua',
+    },
   },
   diagnostics = {
     -- Get the language server to recognize the `vim` global
@@ -12,6 +19,7 @@ local nvim_settings = {
     library = {
       -- Make the server aware of Neovim runtime files
       vim.env.VIMRUNTIME,
+      vim.g.luvit_meta
     },
   },
   telemetry = {
