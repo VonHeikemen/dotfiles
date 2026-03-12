025286acb0aca34fea20a86d2baf0f016a19ad46
Heiker Curiel <heikercuriel@gmail.com>
2026-03-11 22:39:10 -0400

    neovim - disable intelephense telemetry

--- my-configs/neovim/lsp/intelephense.lua
+++ my-configs/neovim/lsp/intelephense.lua
@@ -1,6 +1,11 @@
 return {
   cmd = {'intelephense', '--stdio'},
   filetypes = {'php'},
-  root_markers = {'composer.json'}
+  root_markers = {'composer.json'},
+  settings = {
+    intelephense = {
+      telemetry = {enabled = false},
+    },
+  },
 }
 
