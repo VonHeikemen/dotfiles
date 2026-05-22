3363eeea6898362bf6fe51201d78177237d602ff
Heiker Curiel <heikercuriel@gmail.com>
2026-05-22 07:29:36 -0400

    neovim - update ts-enable config

--- my-configs/neovim/lua/specs/ts-enable.lua
+++ my-configs/neovim/lua/specs/ts-enable.lua
@@ -5,7 +5,16 @@ Plugin.rev = 'v2.x'
 Plugin.opts = {
   auto_install = true,
   highlights = true,
+  regex_syntax = false,
   folds = false,
+  parser_settings = {
+    php = {
+      auto_install = true,
+      highlights = true,
+      regex_syntax = true,
+      folds = false
+    },
+  }
 }
 
 function Plugin.config(opts)
