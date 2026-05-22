1423ffdfde88a6f5484e9e9ba747c1c6481affef
Heiker Curiel <heikercuriel@gmail.com>
2026-05-22 10:04:02 -0400

    neovim - update ts-enable

--- my-configs/neovim/lua/specs/ts-enable.lua
+++ my-configs/neovim/lua/specs/ts-enable.lua
@@ -3,19 +3,26 @@ local Plugin = {'VonHeikemen/ts-enable.nvim'}
 Plugin.rev = 'v2.x'
 
 Plugin.opts = {
-  auto_install = true,
+  auto_install = false,
   highlights = true,
   regex_syntax = false,
-  folds = false,
   parser_settings = {
     php = {
-      auto_install = true,
       highlights = true,
-      regex_syntax = true,
-      folds = false
+      regex_syntax = true
     },
   }
 }
+
+function Plugin.install(event)
+  local exec = 'TSEnableEnsureInstalled'
+  if vim.v.vim_did_enter == 1 then
+    vim.cmd(exec)
+    return
+  end
+
+  vim.api.nvim_create_autocmd('VimEnter', {once = true, command = exec})
+end
 
 function Plugin.config(opts)
   vim.g.ts_enable = opts
